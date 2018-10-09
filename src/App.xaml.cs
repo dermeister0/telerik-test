71fab6a7903c2f1e6ca6e77e3df0a44c1fbad224
Anton Zimin <anton@saritasa.com>
2018-10-09 14:33:22 +0700

    Add test project

---
+++ src/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Test100900
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            Dispatcher.UnhandledException += Dispatcher_UnhandledException;
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void Dispatcher_UnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            Trace.WriteLine($"{DateTime.Now:T} Dispatcher_UnhandledException");
+            e.Handled = true;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            Trace.WriteLine($"{DateTime.Now:T} CurrentDomain_UnhandledException");
+        }
+    }
+}
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.IO;
+using System.Windows;
+using Telerik.Windows.Data;
+
+namespace Test100900
+{
+    internal class TestContext : DbContext
+    {
+        public DbSet<City> Cities { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlite("Data Source=Test.db");
+        }
+    }
+
+    internal class City
+    {
+        public int Id { get; set; }
+    }
+
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private VirtualQueryableCollectionView view;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Load();
+        }
+
+        private void Load()
+        {
+            var context = new TestContext();
+
+            if (File.Exists("Test.db"))
+            {
+                File.Delete("Test.db");
+            }
+
+            File.Copy(@"..\..\Test.db", "Test.db");
+            view = new VirtualQueryableCollectionView(context.Cities);
+            view.FilterDescriptors.Add(new FilterDescriptor("Id", FilterOperator.IsEqualTo, 123));
+            GV.ItemsSource = view;
+            File.Delete("Test.db");
+        }
+    }
+}
